587e25abc370957cd38c61a19ae334f606a2a40f
Yoona8 <elena243v@gmail.com>
2025-12-26 14:58:55 +0100

    feat: Add basic search

--- src/components/EpicReactSearch/EpicReactSearch.tsx
+++ src/components/EpicReactSearch/EpicReactSearch.tsx
@@ -3,14 +3,22 @@
 import { dessertsData } from '@/data/desserts.data';
 import { DessertEntry } from './DessertEntry';
 import styles from './EpicReactSearch.module.css';
+import { useState } from 'react';
 
 export const EpicReactSearch = () => {
+  const [searchQuery, setSearchQuery] = useState('');
+
   return (<div className={styles.wrapper}>
-    <form className={styles.searchForm} action="#" method="GET">
+    <form className={styles.searchForm} >
       <h2>Search Your Favorite</h2>
       <p>
-        <label htmlFor="search">Search:</label>
-        <input id="search" type="search" name="search" />
+        <label htmlFor="search-input">Search:</label>
+        <input 
+          id="search-input" 
+          type="search" 
+          name="search"
+          onChange={(evt) => setSearchQuery(evt.target.value)}
+        />
       </p>
       <fieldset className={styles.fieldset}>
         <legend>Filter by flavor:</legend>
@@ -36,7 +44,10 @@ export const EpicReactSearch = () => {
       <button type="submit">Search</button>
     </form>
     <ul className={styles.desserts}>
-      {dessertsData.map((dessert) => <li key={dessert.id}><DessertEntry dessert={dessert} /></li>)}
+      {dessertsData
+        .filter((dessert) => dessert.name.toLowerCase().includes(searchQuery.toLowerCase()))
+        .map((dessert) => <li key={dessert.id}><DessertEntry dessert={dessert} /></li>)
+      }
     </ul>
   </div>);
 };
